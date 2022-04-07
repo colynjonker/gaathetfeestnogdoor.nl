e3b46bca5f4bd05d12c12ee311d10b2d55fe3e95
ColynJonker <colynjonker@hotmail.com>
2022-04-07 10:03:25 +0200

    Fixed error in fallback.ts data

--- lib/fallback.ts
+++ lib/fallback.ts
@@ -2,7 +2,7 @@ import axios, {AxiosResponse} from "axios";
 
 export const getChartData = async (): Promise<any> => {
     let data = {};
-    fetch('https://gaathetfeestnogdoor.nl/api/charts')
+    fetch('https://gaathetfeestnogdoor.nl/api/chart')
         .then(response => response.json())
         .then(data1 => { console.log(data1); data = data1} );
     console.log(data);
